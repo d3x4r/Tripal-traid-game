372050a42f867fbad1dadaeb1d40966f5ec3cd8e
dexer <norton@e1.ru>
2022-05-01 17:03:04 +0500

    Устраняет дублирование контейнеров в компонентах-страницах

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,14 +1,7 @@
-import Container from '../../components/Container';
 import { ABOUT } from '../../constants/about';
 import Text from '../../components/Text';
 import s from './About.module.scss';
 
-const About = () => (
-    <div className={s.root}>
-        <Container>
-            {ABOUT.split('\n\n\n').map((text, i) => <Text key={i} className={s.text}>{text}</Text>)}
-        </Container>
-    </div>
-);
+const About = () => ABOUT.split('\n\n\n').map((text, i) => <Text key={i} className={s.text}>{text}</Text>);
 
 export default About;
