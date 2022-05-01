4f85d75013d2cfb077423984c9482aa2cbfe89f3
dexer <norton@e1.ru>
2022-05-01 17:39:48 +0500

    Добавляет 404 страницу

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Characters from './pages/Characters';
 import About from './pages/About';
 import Contacts from './pages/Contacts';
 import Layout from './components/Layout';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="characters/:id" element={<Biography />} />
         <Route path="about" element={<About />} />
         <Route path="contacts" element={<Contacts />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes >
   );
---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,6 @@
+import Heading from '../../components/Heading';
+import s from './NotFound.module.scss';
+
+const NotFound = () => <Heading>Not Found 404</Heading>;
+
+export default NotFound;
